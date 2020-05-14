806cba9926ddabe165f8688aa1e042c24c8bf865
thuong <hoathuong0507@gmail.com>
2020-05-14 18:45:16 +0700

    Say hi for the dog

--- Dog.js
+++ Dog.js
@@ -1,6 +1,11 @@
 function Dog() {
+	this.name = 'Dog';
 	this.stomach = [];
 }
 Dog.prototype.eat= function(cat) {
 	this.stomach = push(cat);
 };
+Dog.prototype.msayHi= function() {
+	console.log('Hi, i am dog. My name is'+ Chalk.red(this.name));
+}
+module.exports = Dog;
---
+++ index.js
@@ -0,0 +1,3 @@
+var Dog = require ('./Dog');
+var tem = new Dog(Tom);
+tem.sayHi();
